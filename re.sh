88098c992ab03b419281dbc073cfc5ca09412e14
Philip Rodgers <philip_rodgers@hotmail.com>
2021-02-02 01:07:45 -0600

    Lab 1

--- re.sh
+++ re.sh
@@ -3,6 +3,16 @@
 CONFIG="$1"
 COMMAND="$2"
 
+
+if [ $CONFIG  <> "000-default.conf" ]
+then
+    if [ $CONFIG  <> "default-ssl.conf" ]
+    then
+        exit 1
+    fi
+fi
+
+
 if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
 then
     # Move the current execution state to the proper directory
@@ -17,5 +27,14 @@ then
     sudo service apache2 "$COMMAND"
 else
     echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
+    
+    # List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+    VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+    for FILENAME in $VHOSTS_PATH
+    do
+        echo $FILENAME
+    done   
+    
     exit 1
 fi
